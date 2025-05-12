121c43d4276200a69b1d6374c9411e1e08c0b52c
yeuditg <y0556776631@gmail.com>
2025-05-12 20:20:52 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,3 @@
-
 import { createBrowserRouter } from 'react-router-dom';
 import Login from './Login';
 import LogUp from './LogUp';
@@ -9,50 +8,48 @@ import { useState } from 'react';
 import EditRecipe from './EditRecipe';
 import FilterRecipes from './FilterRecipe';
 
-
-
 const AppRouter = () => {
-    const [loginSuccess, setLoginSuccess] = useState(false);
+const [loginSuccess, setLoginSuccess] = useState(false);
 
-    const handleLoginSuccess = () => {
-        setLoginSuccess(true);
-    };
+const handleLoginSuccess = () => {
+    setLoginSuccess(true);
+};
 
-    const Router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Home />,
-            children: [
-                {
-                    path: 'login',
-                    element: <Login LoginSuccess={handleLoginSuccess} />,
-                },
-                {
-                    path: 'signin',
-                    element: <LogUp LoginSuccess={handleLoginSuccess} />,
-                },
-                {
-                    path: 'showRecipes',
-                    element: <ShowRecipes />,
-                    children: [
-                        {
-                            path: 'addRecipe',
-                            element: <AddRecipe />
-                        },
-                        {
-                            path:"/showRecipes/editRecipe/:id",
-                            element: <EditRecipe />
-                        },
-                        {
-                            path: 'filterRecipes',
-                            element: <FilterRecipes />
-                        },
-                    ],
-                },
-            ],
-        },
-    ]);
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                path: 'login',
+                element: <Login LoginSuccess={handleLoginSuccess} />,
+            },
+            {
+                path: 'signin',
+                element: <LogUp LoginSuccess={handleLoginSuccess} />,
+            },
+            {
+                path: 'showRecipes',
+                element: <ShowRecipes />,
+                children: [
+                    {
+                        path: 'addRecipe',
+                        element: <AddRecipe />
+                    },
+                    {
+                        path:"/showRecipes/editRecipe/:id",
+                        element: <EditRecipe />
+                    },
+                    {
+                        path: 'filterRecipes',
+                        element: <FilterRecipes />
+                    },
+                ],
+            },
+        ],
+    },
+]);
 
-    return Router;
+return Router;
 }
 export default AppRouter;
