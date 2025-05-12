28a4e49e7e5cecffbe1d1689c2b0663205476fa3
yeuditg <y0556776631@gmail.com>
2025-05-12 20:20:15 +0300

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,68 +1,70 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button, Container, Typography, Snackbar } from '@mui/material';
 import React from "react";
-import Login from './Login'; 
+import Login from './Login';
 import ShowRecipes from "./ShowRecipes";
 import LogUp from "./LogUp";
+
 const Home = () => {
-    const nav = useNavigate();
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [showLogUpForm, setShowLogUpForm] = useState(false);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
+const nav = useNavigate();
+const [loginSuccess, setLoginSuccess] = useState(false);
+const [showLoginForm, setShowLoginForm] = useState(false);
+const [showLogUpForm, setShowLogUpForm] = useState(false);
+const [snackbarOpen, setSnackbarOpen] = useState(false);
+const [snackbarMessage, setSnackbarMessage] = useState('');
 
-    const handleLoginSuccess = (success:any) => {
-        setLoginSuccess(success);
-        if (success) {
-            nav("/showRecipes");
-            setSnackbarMessage('התחברות מוצלחת!');
-            setSnackbarOpen(true);
-        }
-    };
+const handleLoginSuccess = (success: boolean) => {
+    setLoginSuccess(success);
+    if (success) {
+        nav("/showRecipes");
+        setSnackbarMessage('התחברות מוצלחת!');
+        setSnackbarOpen(true);
+    }
+};
 
-    const handleSnackbarClose = () => {
-        setSnackbarOpen(false);
-    };
+const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+};
 
-    return (
-        <Container>
-            <Typography color='green' variant="h4" align="center" gutterBottom>
-                ברוכים הבאים למתכונים שלנו
-            </Typography>
-            {!loginSuccess ? (
-                <div className="button-container">
-                    <Button 
-                        variant="contained" 
-                        onClick={() => { 
-                            setShowLoginForm(true); 
-                            setShowLogUpForm(false); 
-                        }}
-                    >
-                        התחבר
-                    </Button>
-                    <Button 
-                        variant="contained" 
-                        onClick={() => { 
-                            setShowLogUpForm(true); 
-                            setShowLoginForm(false); 
-                        }}
-                    >
-                        הירשם
-                    </Button>
-                    {showLoginForm && <Login LoginSuccess={handleLoginSuccess} />}
-                    {showLogUpForm && <LogUp LoginSuccess={handleLoginSuccess} />}
-                </div>
-            ) : <ShowRecipes />}
-            <Snackbar
-                open={snackbarOpen}
-                autoHideDuration={6000}
-                onClose={handleSnackbarClose}
-                message={snackbarMessage}
-            />
-            <Outlet /> 
-        </Container>
-    );
+return (
+    <Container>
+        <Typography color='green' variant="h4" align="center" gutterBottom>
+            ברוכים הבאים למתכונים שלנו
+        </Typography>
+        {!loginSuccess ? (
+            <div className="button-container">
+                <Button 
+                    variant="contained" 
+                    onClick={() => { 
+                        setShowLoginForm(true); 
+                        setShowLogUpForm(false); 
+                    }}
+                >
+                    התחבר
+                </Button>
+                <Button 
+                    variant="contained" 
+                    onClick={() => { 
+                        setShowLogUpForm(true); 
+                        setShowLoginForm(false); 
+                    }}
+                >
+                    הירשם
+                </Button>
+                {showLoginForm && <Login LoginSuccess={handleLoginSuccess} />}
+                {showLogUpForm && <LogUp LoginSuccess={handleLoginSuccess} />}
+            </div>
+        ) : <ShowRecipes />}
+        <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={6000}
+            onClose={handleSnackbarClose}
+            message={snackbarMessage}
+        />
+       <Outlet /> 
+    </Container>
+);
 }
 
 export default Home;
