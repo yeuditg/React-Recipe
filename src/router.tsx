4a237cf66863ce54cb1780534b9aea213ac8c639
yeuditg <y0556776631@gmail.com>
2025-05-12 21:07:19 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -3,53 +3,49 @@ import Login from './Login';
 import LogUp from './LogUp';
 import ShowRecipes from './ShowRecipes';
 import AddRecipe from './AddRecipe';
-import Home from './Home'
+import Home from './Home';
 import { useState } from 'react';
 import EditRecipe from './EditRecipe';
-import FilterRecipes from './FilterRecipe';
 
 const AppRouter = () => {
-const [loginSuccess, setLoginSuccess] = useState(false);
+    const [loginSuccess, setLoginSuccess] = useState(false);
 
-const handleLoginSuccess = () => {
-    setLoginSuccess(true);
-};
+    const handleLoginSuccess = () => {
+        setLoginSuccess(true);
+    };
 
-const Router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        children: [
-            {
-                path: 'login',
-                element: <Login LoginSuccess={handleLoginSuccess} />,
-            },
-            {
-                path: 'signin',
-                element: <LogUp LoginSuccess={handleLoginSuccess} />,
-            },
-            {
-                path: 'showRecipes',
-                element: <ShowRecipes />,
-                children: [
-                    {
-                        path: 'addRecipe',
-                        element: <AddRecipe />
-                    },
-                    {
-                        path:"/showRecipes/editRecipe/:id",
-                        element: <EditRecipe />
-                    },
-                    {
-                        path: 'filterRecipes',
-                        element: <FilterRecipes />
-                    },
-                ],
-            },
-        ],
-    },
-]);
+    const Router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+            children: [
+                {
+                    path: 'login',
+                    element: <Login LoginSuccess={handleLoginSuccess} />,
+                },
+                {
+                    path: 'signin',
+                    element: <LogUp LoginSuccess={handleLoginSuccess} />,
+                },
+                {
+                    path: 'showRecipes',
+                    element: <ShowRecipes />,
+                    children: [
+                        {
+                            path: 'addRecipe',
+                            element: <AddRecipe />
+                        },
+                        {
+                            path: "editRecipe/:id",
+                            element: <EditRecipe />
+                        },
+                    ],
+                },
+            ],
+        },
+    ]);
 
-return Router;
+    return Router;
 }
+
 export default AppRouter;
